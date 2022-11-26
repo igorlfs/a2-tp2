13e99704dc57883e69818cb4cfbbb78b102c078e
Igor <igorlfs@ufmg.br>
2022-11-26 16:42:06 -0300

    fix: docs twice_around_the_tree

--- src/algorithms/algorithms.py
+++ src/algorithms/algorithms.py
@@ -1,9 +1,11 @@
+"""Implementa os algoritmos para solução do caixeiro viajante."""
 import networkx as nx
 
 from src.util import calculate_cost
 
 
 def twice_around_the_tree(graph: nx.Graph) -> float:
+    """Aproxime o caixeiro viajante para `graph` usando o `twice_around_the_tree`."""
     mst: nx.Graph = nx.minimum_spanning_tree(graph)
     vertex_list: list[int] = list(nx.dfs_preorder_nodes(mst, 0))
     return calculate_cost(vertex_list, graph)
